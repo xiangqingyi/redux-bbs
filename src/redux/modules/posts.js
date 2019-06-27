7bdf418d2b5ced36be8e43aeadebe14c8aa50131
xiangqingyi <1352493573@qq.com>
2019-06-27 08:48:44 +0800

    :art: commit

--- src/redux/modules/posts.js
+++ src/redux/modules/posts.js
@@ -134,6 +134,7 @@ const convertPostsToPlain = posts => {
   let postsIds = [];
   let authorsById = {};
   posts.forEach(item => {
+    console.log(item);
     postsById[item.id] = { ...item, author: item.author.id };
     postsIds.push(item.id);
     if (!authorsById[item.author.id]) {
